74c5110d7636df01505216fa763fc17dc93a222d
Inoueyoshito <orainoueyoshito@gmail.com>
2024-08-12 20:07:53 +0900

    途中経過part2

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def blog():
     return render_template('blog.html')
 
+@app.route('/to_know')
+def to_know():
+    return render_template("to_know.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
